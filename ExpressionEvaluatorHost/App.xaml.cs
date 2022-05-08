229c96cb9aaa86523e2b269dd12bdbfaf81fdf92
Aprajit Katiyar <aprajit.katiyar@siemens-healthineers.com>
2022-05-08 15:04:32 +0530

    Created expression evaluator host project

--- ExpressionEvaluatorHost/App.xaml.cs
+++ ExpressionEvaluatorHost/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ExpressionEvaluatorUi
+namespace ExpressionEvaluatorHost
 {
     /// <summary>
     /// Interaction logic for App.xaml
